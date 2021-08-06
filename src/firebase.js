33bfe517927dc7492c26633ee118cc075eb9105b
Vishal <trickyjs091991@gmail.com>
2021-08-06 15:36:18 +0530

    react js contact form

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCTJkwZ4uthi4u5rxLva93GuzGq5e82wQE",
+    authDomain: "contact-form-7d6e9.firebaseapp.com",
+    projectId: "contact-form-7d6e9",
+    storageBucket: "contact-form-7d6e9.appspot.com",
+    messagingSenderId: "632602832596",
+    appId: "1:632602832596:web:fb575beef92f6c0e2f9541"
+  };
+
+  const fireDb = firebase.initializeApp(firebaseConfig);
+
+  export default fireDb.database().ref();
